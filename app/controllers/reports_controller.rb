9e6a746ae0bd1ff8c2ebe0e620dc79c6a3528a52
Nacho <ignacio.palacios.santos@gmail.com>
2017-09-29 20:21:41 +0530

    preparing deploy to staging 16

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -30,4 +30,26 @@ class ReportsController < ApplicationController
       end
     end
   end
+
+  def cache
+    markets = Market.all.map(&:name)
+    @cache_values = []
+
+    markets.each do |market|
+      if market.include?('BTC')
+        prices = CACHE.get(market)
+        if prices.present? && (prices.last != prices.last(2).first)
+          @cache_values << "#{market} - #{prices.last}"
+        end
+      end
+    end
+
+    @title = 'CACHE'
+
+    respond_to do |format|
+      format.pdf do
+        render pdf: @title, template: 'reports/cache.slim'
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'generated-reports/:id/' => 'reports#generate', as: 'generate_reports', :defaults => { :format => 'pdf' }
+  get 'cache/:id/' => 'reports#cache', as: 'cache', :defaults => { :format => 'pdf' }
 
 end
